646a060a99e5ac7aaabbfda398e7a2885637d6f8
Michel Maia <programador.michel.maia@gmail.com>
2022-11-03 18:00:22 -0300

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let email = 'teste@teste.com.br';
+
+email = 'testando@teste.com.br';
+
+console.log('Email')
+console.log('O seu email é: ' + email);
+console.log(`O seu email é:  ${email}`);
+
+
+document.getElementById('btn-submit').addEventListener('click', e => {
+
+    console.log('O botão foi clicado!');
+});
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+    
+    console.log('O mouse está sobre o formulário');
+});
+
+document.getElementById('form-login').addEventListener('mouseout', e => {
+    
+    console.log('O mouse está fora  do formulário');
+});
+
+document.querySelector('#form-login').addEventListener('submit', e => {
+    
+    e.preventDefault();
+
+    //console.log('Formulário enviado!');
+
+    let email = document.querySelector('#email').value; 
+    let password = document.querySelector('#password').value; 
+
+    console.log(email, password);
+});
+
